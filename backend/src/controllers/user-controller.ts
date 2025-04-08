da6af80796c5697257f879a79c11ab5637bce0e1
raghvendra7100 <sharmaraghvendra63@gmail.com>
2025-04-08 21:37:42 +0530

    added the login funcitonality

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,9 +1,9 @@
 import userModel from "../models/userModel";
 import User from "../models/userModel";
 import bcrypt from "bcrypt";
-
-import { signUpSchema } from "../utils/validators";
-
+import { signInSchema, signUpSchema } from "../utils/validators";
+import { createToken } from "../utils/token-manager";
+import { Cookie_Name } from "../utils/constants";
 const saltRounds = 5;
 //api/v1/user
 export const getAllUsers = async(req, res)=>{
@@ -41,7 +41,7 @@ export const signup = async(req, res )=>{
         const user = new User({name , email , password:hashedPassword}) // user instead of userModel.create
 
        await user.save();// create the user in the database ;
-       return res.status(200).json({message : "ok" , id : user._id.toString()})
+       return res.status(201).json({message : "ok" , id : user._id.toString()})
     }catch(error){
          console.log(error);
         return res.status(404).json({
@@ -50,4 +50,68 @@ export const signup = async(req, res )=>{
         
     })
 }
+}
+
+export const login = async(req ,res)=>{
+
+    try{
+        const result  = signInSchema.safeParse(req.body);
+        if(!result.success){
+            const error = result.error;
+            return res.status(400).json("Error while trying to login",
+                error
+            )
+        }
+        
+    const {email , password } = req.body;
+    const response = await User.findOne({
+        email
+    })
+    if(!response){
+        return res.status(400).json({
+            message: "User does not exist."
+        })
+    }
+
+    const passwordMatch = await bcrypt.compare(password , response.password);
+    if(!passwordMatch){
+        return res.status(400).json({
+            message: "Wrong email or password!"
+        })   
+    };
+   
+    res.clearCookie(Cookie_Name , {
+        httpOnly:true,
+        domain: "localhost",
+        signed: true,
+        path:"/"
+    }); // clearing the cookie of previous login if any.
+
+
+   const token = createToken(response._id.toString() , email , "1d")
+   const expires = new Date();
+   expires.setDate(expires.getDate() + 1)
+   
+   res.cookie(Cookie_Name , token , {
+    path:"/",
+    domain:"localhost",
+    expires,
+    httpOnly: true,
+    signed :true
+   })
+   
+   
+   
+   return res.status(200).json({
+        message:"logged in"
+    })
+
+    
+    }catch(error){
+        console.log("Login error:", error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            cause: error.message
+        });
+}   
 }
