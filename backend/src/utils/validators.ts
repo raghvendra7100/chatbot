d041595d8aac5e3b96da7eff3e4d8ef8e7f44246
raghvendra7100 <sharmaraghvendra63@gmail.com>
2025-04-08 21:42:24 +0530

    added zod validation for signup and login format

--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -4,5 +4,10 @@ import { z } from "zod";
 export const signUpSchema =z.object({
     name:z.string().min(3 ,"Name is Reqired").max(100),
     email:z.string().min(3).max(100).email("invalid email"),
-    password:z.string().min(8 , "password must be minimum 6 characters")
+    password:z.string().min(8 , "password must be minimum 6 characters").max(100 , "password must not exceed 100 characters")
 });
+
+export const signInSchema  = z.object({
+    email:z.string().min(3).max(100).email("invalid email"),
+    password:z.string().min(8 , "passaword must be minimus of 8 characters").max(100, "password must not exceed 100 characters")
+})
